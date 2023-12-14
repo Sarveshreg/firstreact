9356d3ce05677486b4c259370536ea6a3c10d8a8
Sarveshreg <sarveshregmi1@gmail.com>
2023-12-13 22:40:00 -0600

    12/13/2023

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { puppyList } from './data.js'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId,setFeatPupId]= useState(null)
+  console.log("puppieees"+puppyList);
+  // function handleClick(p){
+  //   console.log("puppy id: "+p.id)
+  // }
+  let featuredPup= puppies.find((puppy)=>puppy.id===featPupId);
+  console.log("featured pup",featuredPup);
+  return (
+    <>
+      <div>
+        <div id='mainList'>
+        {
+          puppies.map((puppy)=>{
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+        </div>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      {/* <h1>Vite + React</h1> */}
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
